efb39815be1f990c35625adc3f680e6659bc60fb
Benedikt Rascher-Friesenhausen <benediktrascherfriesenhausen+github@gmail.com>
2016-02-10 19:36:32 +0100

    admin: Create custom admin for MTGCard

--- admin.py
+++ admin.py
@@ -12,11 +12,59 @@ from .models import (
 )
 
 
+@admin.register(MTGCard)
+class MTGCardAdmin(admin.ModelAdmin):
+    fieldsets = (
+        ('Set', {
+            'fields': ('multiverseid', ('mtgset', 'set_number',
+                                        'set_number_suffix'))
+        }),
+        ('Card', {
+            'fields': ('name', 'types', 'rules', 'flavour')
+        }),
+        ('Mana', {
+            'fields': (('mana_n', 'mana_w', 'mana_u', 'mana_b', 'mana_r',
+                       'mana_g', 'mana_c'), ('mana_special', 'cmc'))
+        }),
+        ('Stats', {
+            'fields': (('power', 'toughness', 'loyalty'),
+                       ('power_special', 'toughness_special',
+                       'loyalty_special'))
+        }),
+        ('Rarity', {
+            'fields': ['rarity']
+        }),
+        ('Dual Card', {
+            'classes': ('collapse',),
+            'fields': [('dual_type', 'dual_card')]
+        }),
+        ('Legality', {
+            'classes': ('collapse',),
+            'fields': [('legal_vintage', 'legal_legacy', 'legal_extended',
+                        'legal_standard', 'legal_classic', 'legal_commander',
+                        'legal_modern')]
+        }),
+        ('Rulings', {
+            'classes': ('collapse',),
+            'fields': ['rulings']
+        }),
+        ('Artist', {
+            'fields': ['artist']
+        }),
+    )
+    filter_horizontal = ['rulings']
+    list_display = ('name', 'types', 'cmc', 'power', 'toughness', 'rarity',
+                    'mtgset')
+    list_filter = ('rarity', 'mtgset', 'dual_type',
+                   'legal_standard', 'legal_modern')
+    search_fields = ('name', 'types')
+
+
 # Register your models here.
 admin.site.register(Artist)
 admin.site.register(MTGBlock)
 admin.site.register(MTGSet)
-admin.site.register(MTGCard)
+# admin.site.register(MTGCard)
 admin.site.register(MTGRuling)
 admin.site.register(MTGCollection)
 admin.site.register(MTGToken)
