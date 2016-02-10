09798b2da1c1cc5b3555c30b886cb968293b1e46
Benedikt Rascher-Friesenhausen <benediktrascherfriesenhausen+github@gmail.com>
2016-02-10 18:07:51 +0100

    models: Create abstract base models
    Create base models for cards/tokens and collection entries.

--- models/mtg.py
+++ models/mtg.py
@@ -15,6 +15,15 @@ class Artist(models.Model):
         return '{0} {1}'.format(self.first_name, self.last_name)
 
 
+class MTGRuling(models.Model):
+    """Model for rulings that affect certain `tccm.models.MTGCard`s."""
+    ruling = models.TextField(unique=True)
+    date = models.DateField("date of the ruling")
+
+    def __str__(self):
+        return '{0}: {1}'.format(self.date, self.ruling[:50])
+
+
 class MTGBlock(models.Model):
     """Model for a block in Magic The Gathering.  Used to group
     `tccm.models.MTGSet`.
@@ -46,8 +55,8 @@ class MTGSet(models.Model):
         return self.name
 
 
-class MTGCard(models.Model):
-    """Model for a card in Magic The Gathering."""
+class MTGBaseCard(models.Model):
+    """The base model for a card from Magic The Gathering."""
     # === set ========================================================
     set_code = models.ForeignKey(MTGSet, on_delete=models.CASCADE)
     set_number_suffix = models.CharField(max_length=1, blank=True, default='')
@@ -59,6 +68,24 @@ class MTGCard(models.Model):
     rules = models.TextField(blank=True)
     flavour = models.TextField(blank=True)
 
+    # === stats ======================================================
+    power = models.PositiveSmallIntegerField(default=0)
+    power_special = models.CharField(max_length=10, blank=True, default='')
+    toughness = models.PositiveSmallIntegerField(default=0)
+    toughness_special = models.CharField(max_length=10, blank=True,
+                                         default='')
+    loyalty = models.PositiveSmallIntegerField(default=0)
+    loyalty_special = models.CharField(max_length=10, blank=True, default='')
+
+    # === art ========================================================
+    artist = models.ForeignKey(Artist, on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        abstract = True
+
+
+class MTGCard(MTGBaseCard):
+    """Model for a card in Magic The Gathering."""
     # === mana =======================================================
     mana_n = models.PositiveSmallIntegerField("neutral mana", default=0)
     mana_w = models.PositiveSmallIntegerField("white mana", default=0)
@@ -70,33 +97,23 @@ class MTGCard(models.Model):
     mana_special = models.CharField("special mana", max_length=50, blank=True)
     cmc = models.PositiveSmallIntegerField("converted mana cost", default=0)
 
-    # === stats ======================================================
-    power = models.PositiveSmallIntegerField(default=0)
-    power_special = models.CharField(max_length=10, blank=True, default='')
-    toughness = models.PositiveSmallIntegerField(default=0)
-    toughness_special = models.CharField(max_length=10, blank=True,
-                                         default='')
-    loyalty = models.PositiveSmallIntegerField(default=0)
-    loyalty_special = models.CharField(max_length=10, blank=True, default='')
-
     # === rarity =====================================================
     RARITY_MYTHIC_RARE = 'M'
     RARITY_RARE = 'R'
     RARITY_UNCOMMON = 'U'
     RARITY_COMMON = 'C'
     RARITY_SPECIAL = 'S'
+    RARITY_LAND = 'L'
     RARITIES = (
         (RARITY_MYTHIC_RARE, 'Mythic Rare'),
         (RARITY_RARE, 'Rare'),
         (RARITY_UNCOMMON, 'Uncommon'),
         (RARITY_COMMON, 'Common'),
         (RARITY_SPECIAL, 'Special'),
+        (RARITY_LAND, 'Land'),
     )
     rarity = models.CharField(max_length=1, choices=RARITIES,
                               default=RARITY_COMMON)
-
-    # === art ========================================================
-    artist = models.ForeignKey(Artist, on_delete=models.SET_NULL, null=True)
 
     # === dual card ==================================================
     DUAL_NONE = ''
@@ -111,6 +128,9 @@ class MTGCard(models.Model):
                                  default=DUAL_NONE, blank=True)
     dual_card = models.OneToOneField('self', on_delete=models.SET_NULL,
                                      default=None, null=True, blank=True)
+
+    # === rulings ====================================================
+    rulings = models.ManyToManyField(MTGRuling)
 
     # === legality ===================================================
     LEGALITY_NONE = ''
@@ -146,24 +166,53 @@ class MTGCard(models.Model):
         return self.name
 
 
-class MTGRuling(models.Model):
-    """Model for rulings that affect certain `tccm.models.MTGCard`s."""
-    cards = models.ManyToManyField(MTGCard)
-    ruling = models.TextField(unique=True)
-    date = models.DateField("date of the ruling")
+class MTGToken(MTGBaseCard):
+    """Model of a token in Magic The Gatherin."""
+    # === rarity =====================================================
+    RARITY_NONE = ''
+    RARITY_TOKEN = 'T'
+    RARITY_EMBLEM = 'E'
+    RARITY_OTHER = 'O'
+    RARITIES = (
+        (RARITY_NONE, 'No rarity'),
+        (RARITY_TOKEN, 'Token'),
+        (RARITY_EMBLEM, 'Emblem'),
+        (RARITY_OTHER, 'Other'),
+    )
+    rarity = models.CharField(max_length=1, choices=RARITIES, blank=True,
+                              default=RARITY_NONE)
+
+    def __str__(self):
+        return self.name
 
 
 class MTGCollection(models.Model):
     """Model of a shareable collection of `tccm.models.MTGCard`s."""
     name = models.CharField(max_length=100)
-    cards = models.ManyToManyField(MTGCard, through='MTGCollectionEntry')
+    cards = models.ManyToManyField(MTGCard, through='MTGCollectionCardEntry')
+    tokens = models.ManyToManyField(MTGToken, through='MTGCollectionTokenEntry')
     date_created = models.DateField()
 
+    # === user =======================================================
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     viewers = models.ManyToManyField(User,
                                      related_name='viewable_mtgcollections')
     editors = models.ManyToManyField(User,
                                      related_name='editable_mtgcollections')
+
+    # === lands ======================================================
+    lands_plains = models.PositiveSmallIntegerField("number of basic plains",
+                                                    default=0)
+    lands_island = models.PositiveSmallIntegerField("number of basic islands",
+                                                    default=0)
+    lands_swamp = models.PositiveSmallIntegerField("number of basic swamps",
+                                                   default=0)
+    lands_mountain = models.PositiveSmallIntegerField("number of basic "
+                                                      "mountains", default=0)
+    lands_forest = models.PositiveSmallIntegerField("number of basic forests",
+                                                    default=0)
+    lands_wastes = models.PositiveSmallIntegerField("number of basic wastes",
+                                                    default=0)
 
     class Meta:
         ordering = ['date_created']
@@ -175,12 +224,27 @@ class MTGCollection(models.Model):
 class MTGCollectionEntry(models.Model):
     """Model of a single entry of a `tccm.models.MTGCollection`."""
     collection = models.ForeignKey(MTGCollection, on_delete=models.CASCADE)
-    card = models.ForeignKey(MTGCard, on_delete=models.CASCADE)
     count = models.PositiveSmallIntegerField("number of copies in the "
                                              "collection", default=1)
+
+    class Meta:
+        abstract = True
+
+
+class MTGCollectionCardEntry(MTGCollectionEntry):
+    """Model a single card entry of a `tccm.models.MTGCollection`."""
+    card = models.ForeignKey(MTGCard, on_delete=models.CASCADE)
     foil_count = models.PositiveSmallIntegerField("number of foiled "
                                                   "copies in the collection.",
                                                   default=0)
 
     def __str__(self):
         return '{0} in {1}'.format(self.card.name, self.collection.name)
+
+
+class MTGCollectionTokenEntry(MTGCollectionEntry):
+    """Model a single token entry of a `tccm.models.MTGCollection`."""
+    token = models.ForeignKey(MTGToken, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '{0} in {1}'.format(self.token.name, self.collection.name)
