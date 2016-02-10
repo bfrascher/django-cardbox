90923e61df4a7f185b22dfbf643ae7de5a827e29
Benedikt Rascher-Friesenhausen <benediktrascherfriesenhausen+github@gmail.com>
2016-02-10 16:18:31 +0100

    Register models with admin

--- admin.py
+++ admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
+from .models import (
+    Artist,
+    MTGBlock,
+    MTGSet,
+    MTGCard,
+    MTGRuling,
+    MTGCollection,
+#    MTGCollectionEntry,
+)
+
+
 # Register your models here.
+admin.site.register(Artist)
+admin.site.register(MTGBlock)
+admin.site.register(MTGSet)
+admin.site.register(MTGCard)
+admin.site.register(MTGRuling)
+admin.site.register(MTGCollection)
