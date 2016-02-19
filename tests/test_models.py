56d49cc2a4865983a4f0973923fe1fe672dad9de
Benedikt Rascher-Friesenhausen <benediktrascherfriesenhausen+github@gmail.com>
2016-02-19 16:57:52 +0100

    tests: Correct name of test card

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,3 +1,5 @@
+# coding: utf-8
+
 import pytest
 
 from cardbox.models import (
@@ -10,6 +12,91 @@ from cardbox.models import (
     Collection,
     CollectionEntry,
 )
+
+
+class MockParser:
+    class MockParser:
+    blocks = [
+        Block(name='Test block', category=Block.CATEGORY_NONE),
+    ]
+
+    sets = [
+        Set(code='ORI', name='Magic Origins'),
+        Set(code='DKA', name='Dark Ascension'),
+    ]
+
+    setentries = {}
+    setentries['ORI'] = [
+        (
+            CardEdition(number=80, number_suffix='',
+                        rarity=CardEdition.RARITY_UNCOMMON),
+            Card(name='Tower Geist', types='Creature — Spirit'),
+            Artist(first_name='', last_name='Izzy'),
+            [],
+        ),
+        (
+            CardEdition(number=60, number_suffix='a',
+                        rarity=CardEdition.RARITY_MYTHIC_RARE),
+            Card(name="Jace, Vryn's Prodigy",
+                 types='Legendary Creature — Human Wizard',
+                 multi_type=Card.MULTI_FLIP),
+            Artist(first_name='Jaime', last_name='Jones'),
+            [],
+        ),
+        (
+            CardEdition(number=60, number_suffix='b',
+                        rarity=CardEdition.RARITY_MYTHIC_RARE),
+            Card(name='Jace, Telepath Unbound', types='Planeswalker — Jace',
+                 multi_type=Card.MULTI_FLIP),
+            Artist(first_name='Jaime', last_name='Jones'),
+            [],
+        ),
+        (
+            CardEdition(number=261, number_suffix='',
+                        rarity=CardEdition.RARITY_LAND),
+            Card(name='Swamp', types='Basic Land — Swamp'),
+            Artist(first_name='Larry', last_name='Elmore'),
+            []
+        ),
+        (
+            CardEdition(number=262, number_suffix='',
+                        rarity=CardEdition.RARITY_LAND),
+            Card(name='Swamp', types='Basic Land — Swamp'),
+            Artist(first_name='Dan', last_name='Frazier'),
+            []
+        ),
+        (
+            CardEdition(number=264, number_suffix='',
+                        rarity=CardEdition.RARITY_LAND),
+            Card(name='Swamp', types='Basic Land — Swamp'),
+            Artist(first_name='Jung', last_name='Park'),
+            []
+        ),
+    ]
+    setentries['DKA'] = [
+        (
+            CardEdition(number=53, number_suffix='',
+                        rarity=CardEdition.RARITY_UNCOMMON),
+            Card(name='Tower Geist', types='Creature — Spirit'),
+            Artist(first_name='', last_name='Izzy'),
+            [],
+        ),
+        (
+            CardEdition(number=3457, number_suffix='no',
+                        rarity=CardEdition.RARITY_SPECIAL),
+            Card(name='No card', types='None'),
+            Artist(first_name='', last_name='Izzy'),
+            [],
+        ),
+    ]
+
+    def parse_blocks_sets():
+        for block in MockParser.blocks:
+            yield block, MockParser.sets
+
+    def parse_cards_by_set(setcode):
+        for edition, card, artist, rulings in MockParser.setentries[setcode]:
+            yield edition, card, artist, rulings
 
 
 class TestCard:
@@ -57,3 +144,12 @@ class TestCard:
                     toughness=t, toughness_special=ts,
                     loyalty=l, loyalty_special=ls)
         assert card.get_ptl() == ptl
+
+    def test_get_newest_edition(self):
+        pass
+
+    def test_get_image_url(self):
+        pass
+
+    def test_get_count_in_collection(self):
+        pass
