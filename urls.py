eb3fe6d0e723e785b9c7787d78f36916625d2e3d
Benedikt Rascher-Friesenhausen <benediktrascherfriesenhausen+github@gmail.com>
2016-02-19 13:17:46 +0100

    urls: Change url for adding a new collection

--- urls.py
+++ urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     url(r'^logout/$', views.logout_view, name='logout'),
     url(r'^cards/(?P<layout>[a-z]+)/$', views.cards, name='cards'),
     url(r'^card/(?P<card_id>[0-9]+)/$', views.card, name='card'),
+    url(r'^collection/new/$', views.edit_collection, name='new_collection'),
     url(r'^collection/edit/(?P<collection_id>[0-9]+)/$', views.edit_collection, name='edit_collection'),
     url(r'^collection/delete/(?P<collection_id>[0-9]+)/$', views.delete_collection, name='delete_collection'),
     url(r'^collection/(?P<collection_id>[0-9]+)/add$', views.add_collection_entry, name='add_collection_entry'),
