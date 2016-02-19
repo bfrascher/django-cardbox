8c9bb97c54805e572dbbcac4893846d74c383865
Benedikt Rascher-Friesenhausen <benediktrascherfriesenhausen+github@gmail.com>
2016-02-19 20:08:40 +0100

    tests: Add tests for models.Card/CardEdition

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,6 +1,9 @@
 # coding: utf-8
 
+import datetime
 import pytest
+
+from django.contrib.auth.models import User
 
 from cardbox.models import (
     Artist,
@@ -13,16 +16,22 @@ from cardbox.models import (
     CollectionEntry,
 )
 
+from cardbox.utils.db import (
+    insert_blocks_sets_cards_from_parser,
+)
+
+
 
 class MockParser:
-    class MockParser:
     blocks = [
         Block(name='Test block', category=Block.CATEGORY_NONE),
     ]
 
     sets = [
-        Set(code='ORI', name='Magic Origins'),
-        Set(code='DKA', name='Dark Ascension'),
+        Set(code='ORI', name='Magic Origins',
+            release_date=datetime.date(2015, 7, 17)),
+        Set(code='DKA', name='Dark Ascension',
+            release_date=datetime.date(2012, 2, 3)),
     ]
 
     setentries = {}
@@ -99,6 +108,61 @@ class MockParser:
             yield edition, card, artist, rulings
 
 
+@pytest.mark.django_db
+class PrepareCollections:
+    user = [
+        User(username='number one'),
+        User(username='darling'),
+    ]
+    collections = [
+        (
+            'number one',
+            Collection(name='First collection', date_created=datetime.date(1, 1, 1)),
+        ),
+        (
+            'darling',
+            Collection(name='Bestest', date_created=datetime.date(1, 1, 1)),
+        ),
+    ]
+
+    entries = {}
+    entries['First collection'] = [
+        ('ORI', 60, 'a', 5, 2),
+        ('ORI', 80, '', 3, 0),
+        ('DKA', 53, '', 0, 2),
+        ('ORI', 262, '', 5, 0),
+        ('ORI', 264, '', 2, 0),
+    ]
+    entries['Bestest'] = [
+        ('ORI', 60, 'b', 2, 7),
+        ('ORI', 80, '', 8, 2),
+        ('DKA', 53, '', 3, 77),
+        ('ORI', 262, '', 2, 0),
+        ('ORI', 264, '', 0, 1),
+    ]
+
+    def create_collections():
+        """Prepare some collections and collection entries."""
+        insert_blocks_sets_cards_from_parser(parser=MockParser)
+        for user in PrepareCollections.user:
+            user.save()
+
+        for username, collection in PrepareCollections.collections:
+            user = User.objects.get(username=username)
+            collection.owner = user
+            collection.save()
+
+            for (code, number, number_suffix, count,
+                 foil_count) in PrepareCollections.entries[collection.name]:
+                edition = CardEdition.objects.get(
+                    mtgset__code=code, number=number,
+                    number_suffix=number_suffix)
+                entry = CollectionEntry(collection=collection, edition=edition,
+                                        count=count, foil_count=foil_count)
+                entry.save()
+
+
+
 class TestCard:
     @pytest.mark.parametrize("mana,mana_n,mana_w,mana_u,mana_b,mana_r,mana_g,mana_c,mana_special", [
         ('10RG', 10, None, None, None, 1, 1, None, ''),
@@ -145,11 +209,58 @@ class TestCard:
                     loyalty=l, loyalty_special=ls)
         assert card.get_ptl() == ptl
 
-    def test_get_newest_edition(self):
-        pass
+    @pytest.mark.django_db
+    @pytest.mark.parametrize("cardname,code,number,number_suffix", [
+        ('Tower Geist', 'ORI', 80, ''),
+        ('No card', 'DKA', 3457, 'no'),
+        ('Swamp', 'ORI', 264, ''),
+    ])
+    def test_get_newest_edition(self, cardname, code, number, number_suffix):
+        insert_blocks_sets_cards_from_parser(parser=MockParser)
+        card = Card.objects.get(name=cardname)
+        edition = CardEdition.objects.get(mtgset__code=code, number=number,
+                                          number_suffix=number_suffix)
+        assert edition == card.get_newest_edition()
 
-    def test_get_image_url(self):
-        pass
+    @pytest.mark.django_db
+    @pytest.mark.parametrize("url,cardname", [
+        ('cardbox/images/cards/ORI/60b.jpg', 'Jace, Telepath Unbound'),
+        ('cardbox/images/cards/ORI/80.jpg', 'Tower Geist'),
+        ('cardbox/images/cards/DKA/3457no.jpg', 'No card'),
+    ])
+    def test_get_image_url(self, url, cardname):
+        insert_blocks_sets_cards_from_parser(parser=MockParser)
+        card = Card.objects.get(name=cardname)
+        assert url == card.get_image_url()
 
-    def test_get_count_in_collection(self):
-        pass
+    @pytest.mark.django_db
+    @pytest.mark.parametrize("collectionname,cardname,count,fcount", [
+        ('First collection', "Jace, Vryn's Prodigy", 5, 2),
+        ('First collection', "Tower Geist", 3, 2),
+        ('First collection', "Swamp", 7, 0),
+        ('Bestest', "Jace, Telepath Unbound", 2, 7),
+        ('Bestest', "Tower Geist", 11, 79),
+        ('Bestest', "Swamp", 2, 1),
+    ])
+    def test_get_count_in_collection(self, collectionname, cardname,
+                                     count, fcount):
+        PrepareCollections.create_collections()
+
+        card = Card.objects.get(name=cardname)
+        collection = Collection.objects.get(name=collectionname)
+        c, fc = card.get_count_in_collection(collection)
+        assert c == count
+        assert fc == fcount
+
+
+class TestCardEdition:
+    """All tests for methods of :cls:`cardbox.models.CardEdition`."""
+    @pytest.mark.parametrize("number_str,number,number_suffix", [
+        ('435c', 435, 'c'),
+        ('54', 54, ''),
+        pytest.mark.xfail(('3abc', 3, 'c')),
+    ])
+    def test_parse_number(self, number_str, number, number_suffix):
+        n, ns = CardEdition.parse_number(number_str)
+        assert n == number
+        assert ns == number_suffix
